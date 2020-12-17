f00b821317a8637f8d707751cef63f8c17d48bc2
Paco Shum <paco.shum@jda.com>
2020-12-17 11:41:12 +0000

    pwd_check

--- process_register.php
+++ process_register.php
@@ -29,27 +29,38 @@ $surname = $_POST['txtSurname'];
 $email = $_POST['txtEmail']; 
 $password = $_POST['txtPassword']; 
 
+
 if (($username !== '' )&&($forname !== '' )&&($surname !== '' )&&($email !== '' )&&($password !== '')) {
-    //password encryption
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-    //  INSERT query   , check hash variable in the Values statement 
-    $stmt = $conn->prepare("INSERT INTO systemuser (Username, Password, Forename, Surname, Email) Values(? , ? , ? , ? , ?)");
-    $stmt->bind_param("sssss", $username, $passwordHash, $forname, $surname, $email);
-    //$userQuery = "INSERT INTO systemuser (Username, Password, Forename, Surname, Email) Values('$username', '$passwordHash', '$forname', '$surname', '$email')";
-
-    if ($stmt->execute())
-    {
-        session_regenerate_id();
-        $_SESSION['registered'] = TRUE;
-        $_SESSION['name'] = $_POST['txtUsername'];
-        $_SESSION['id'] = $id;
-        header('Location: submitted.php');
-    }
-    else
-    {
-        $_SESSION['registered_error'] = "Somethings Wrong";
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+    if((!preg_match("#[a-zA-Z]+#", $password)) || (!preg_match("#[0-9]+#", $password)) || (strlen($password) < 8)) {
+        $_SESSION['registered_error'] = "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
         header('Location: index.php');
+    }
+    else {
+        //password encryption
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+        //  INSERT query   , check hash variable in the Values statement 
+        $stmt = $conn->prepare("INSERT INTO systemuser (Username, Password, Forename, Surname, Email) Values(? , ? , ? , ? , ?)");
+        $stmt->bind_param("sssss", $username, $passwordHash, $forname, $surname, $email);
+        //$userQuery = "INSERT INTO systemuser (Username, Password, Forename, Surname, Email) Values('$username', '$passwordHash', '$forname', '$surname', '$email')";
+
+        if ($stmt->execute())
+        {
+            session_regenerate_id();
+            $_SESSION['registered'] = TRUE;
+            $_SESSION['name'] = $_POST['txtUsername'];
+            $_SESSION['id'] = $id;
+            header('Location: submitted.php');
+        }
+        else
+        {
+            $_SESSION['registered_error'] = "Somethings Wrong";
+            header('Location: index.php');
+        }
     }
 }else{
     $_SESSION['registered_error'] = "Somethings Wrong";
